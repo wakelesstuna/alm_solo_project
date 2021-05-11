3862f5151a297d35862ed274582e43d31f2afb27
Oscar Forss <oscar.forss.1@gmail.com>
2021-05-11 10:56:03 +0200

    Add car model

--- src/main/java/com/example/springdocker/model/Car.java
+++ src/main/java/com/example/springdocker/model/Car.java
@@ -1,2 +1,19 @@
-package com.example.springdocker.model;public class Car {
+package com.example.springdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Car {
+
+    String id;
+    String brand;
+    String model;
+    int modelYear;
+
 }
