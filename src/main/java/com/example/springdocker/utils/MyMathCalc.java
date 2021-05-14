6996b3b83cda195619d1463f761861551bc61ee2
Oscar Forss <oscar.forss.1@gmail.com>
2021-05-14 09:31:47 +0200

    Add test for MyMathCalc

--- src/main/java/com/example/springdocker/utils/MyMathCalc.java
+++ src/main/java/com/example/springdocker/utils/MyMathCalc.java
@@ -10,7 +10,12 @@ public class MyMathCalc {
         return a * b;
     }
 
-    public double divide(int a, int b){
-        return a / b;
+    public double divide(double numberToDivide, double divideWith){
+        if (divideWith == 0){
+            throw new NumberFormatException("You can't divide with 0");
+        }else {
+            return numberToDivide / divideWith;
+        }
+
     }
 }
---
+++ src/test/java/com/example/springdocker/utils/MyMathCalcTest.java
@@ -0,0 +1,64 @@
+package com.example.springdocker.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyMathCalcTest {
+
+    private MyMathCalc myMathCalc;
+
+    @BeforeEach
+    void start() {
+        myMathCalc = new MyMathCalc();
+    }
+
+    @Test
+    void testAddition(){
+        int expected = 2;
+        int notExpected = 1000;
+        int actual = myMathCalc.add(1,1);
+
+        assertEquals(expected,actual);
+        assertNotEquals(notExpected,actual);
+    }
+
+    @Test
+    void testMultiply() {
+        int expected = 10;
+        int notExpected = 1000;
+        int actual = myMathCalc.multiply(2,5);
+
+        assertEquals(expected,actual);
+        assertNotEquals(notExpected,actual);
+    }
+
+    @Test
+    void testDivide() {
+        int expected = 5;
+        int notExpected = 1000;
+        double actual = myMathCalc.divide(10,2);
+
+        assertEquals(expected,actual);
+        assertNotEquals(notExpected,actual);
+    }
+
+    @Test
+    void testDivide2() {
+        double expected = 0.2;
+        int notExpected = 1000;
+        double actual = myMathCalc.divide(2,10);
+
+        assertEquals(expected,actual);
+        assertNotEquals(notExpected,actual);
+    }
+
+    @Test
+    void testDivide3() {
+        assertThrows(NumberFormatException.class,() -> myMathCalc.divide(10,0));
+    }
+
+
+
+}
